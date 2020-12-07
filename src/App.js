38654279aecb740c0ee032245d7f84109949affe
Aoda <hinokamiaoda@gmail.com>
2020-12-08 01:41:00 +0600

    added HashRouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { HashRouter as Router, Switch, Route } from 'react-router-dom'
 
 import DeskList from './components/DeskList/DeskList'
 import ColumnList from './components/ColumnList/ColumnList'
